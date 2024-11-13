b544b137b86895802a1faa118a3cbc21a12b4bbf
isPimenov <ipimenow@gmail.com>
2024-11-13 22:21:22 +0300

    Update Module_12_4.py

--- Module_12_4.py
+++ Module_12_4.py
@@ -2,18 +2,25 @@ import logging
 from HumanMoveTest import Runner
 import unittest
 
+logging.basicConfig(level=logging.INFO,
+                    filemode='w',
+                    filename='runner_tests.log',
+                    encoding='utf-8',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 class RunnerTest(unittest.TestCase):
     def test_walk(self):
         try:
-            logging.info('"test_walk" выполнен успешно')
             runner = Runner('Bob', -5)
             for i in range(10):
                 runner.walk()
             self.assertEqual(runner.distance, 50)
+            logging.info('"test_walk" выполнен успешно')
         except ValueError as err:
             logging.warning('Неверная скорость для Runner')
-            #raise ValueError(f'Скорость не может быть отрицательной, сейчас')
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас')
+
 
     def test_run(self):
         try:
@@ -24,7 +31,7 @@ class RunnerTest(unittest.TestCase):
             logging.info('"test_run" выполнен успешно')
         except TypeError:
             logging.warning('Неверный тип данных для объекта Runner')
-            #raise TypeError(f'Имя должно быть строкой, передано {type(runner.name).__name__}')
+            raise TypeError(f'Имя должно быть строкой, передано {type(runner.name).__name__}')
 
     def test_challenge(self):
         runner_1 = Runner('Bob')
@@ -36,10 +43,4 @@ class RunnerTest(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO,
-                        filemode='w',
-                        filename='runner_tests.log',
-                        #encoding='utf-8',
-                        format='%(asctime)s - %(levelname)s - %(message)s')
-
     unittest.main()
